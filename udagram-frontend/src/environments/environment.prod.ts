f2e65d43a13123622d9849364ff0dbd7a1c869ca
felipass <felipass@gmail.com>
2021-12-29 13:54:27 +0000

    add changes to env and package.json

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
- // apiHost: 'http://localhost:8080/api/v0'
-  apiHost: 'http://a4fe7b66b2d01468fb5cdc976175ba53-1069253399.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
+  //apiHost: 'http://a4fe7b66b2d01468fb5cdc976175ba53-1069253399.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
